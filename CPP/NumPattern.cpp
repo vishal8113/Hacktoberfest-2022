ce8c1c252f853a20110c0c76e7d49985d6be6cc2
Shubham Parmar <shubhamparmarmoney@gmail.com>
2022-10-12 19:40:24 +0530

    Added interview question printing pattern of numbers

---
+++ CPP/NumPattern.cpp
@@ -0,0 +1,28 @@
+/*
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int rows;
+
+    cout << "Enter number of rows: ";
+    cin >> rows;
+
+    for(int i = 1; i <= rows; ++i)
+    {
+        for(int j = 1; j <= i; ++j)
+        {
+            cout << j << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
