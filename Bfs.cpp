3ae6a9ca1687d215f270f4dfc8c8a8e1b2048f53
Manmeetkaur1525 <kaurmanmeet1525@gmail.com>
2022-10-26 16:26:57 +0530

    bfs for graph

--- Bfs.cpp
+++ Bfs.cpp
@@ -1,39 +1,86 @@
-#include<iostream>
-#include<conio.h>
-#include<stdlib.h>
-int cost[10][10],i,j,k,n,qu[10],front,rare,v,visit[10],visited[10];
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<bool> v;
+vector<vector<int>> g;
+
+void bfsTraversal(int b)
+{
+    //Declare a queue to store all the nodes connected to b
+    queue<int> q;
+
+    //Insert b to queue
+    q.push(b);
+
+    //mark b as visited
+    v[b] = true;
+
+    cout << "\n\nThe BFS Traversal is:  ";
+
+    while (!q.empty())
+    {
+        int a = q.front();
+        q.pop(); //delete the first element form queue
+
+        for (auto j = g[a].begin(); j != g[a].end(); j++)
+        {
+            if (!v[*j])
+            {
+                v[*j] = true;
+                q.push(*j);
+            }
+        }
+        cout << a << "  ";
+    }
+}
+
+void makeEdge(int a, int b)
+{
+    g[a].push_back(b); //an edge from a to b (directed graph)
+}
+
 int main()
 {
-    int m;
-    cout <<"Enter no of vertices:";
+    cout << "\n\nWelcome to Studytonight :-)\n\n\n";
+    cout << " =====  Program to demonstrate the Breadth First Search Algorithm, in CPP  ===== \n\n";
+
+    cout << " =====  Note; The vertices are numbered from 0 to n-1.  ===== \n\n";
+
+    int n, e;
+
+    cout << "Enter the number of vertices: ";
+
     cin >> n;
-    cout <<"Enter no of edges:";
-    cin >> m;
-    cout <<"\nEDGES \n";
-    for(k=1; k<=m; k++)
+
+    cout << "\n\nEnter the number of edges: ";
+
+    cin >> e;
+
+    v.assign(n, false);
+    g.assign(n, vector<int>());
+
+    int a, b, i;
+
+    cout << "Enter the edges with source and target vetex: \n ";
+
+    for (i = 0; i < e; i++)
     {
-        cin >>i>>j;
-        cost[i][j]=1;
+        cin >> a >> b;
+        makeEdge(a, b);
     }
-    cout <<"Enter initial vertex to traverse from:";
-    cin >>v;
-    cout <<"Visitied vertices:";
-    cout <<v<<" ";
-    visited[v]=1;
-    k=1;
-    while(k<n)
+
+    for (i = 0; i < n; i++)
     {
-        for(j=1; j<=n; j++)
-            if(cost[v][j]!=0 && visited[j]!=1 && visit[j]!=1)
-            {
-                visit[j]=1;
-                qu[rare++]=j;
-            }
-        v=qu[front++];
-        cout<<v <<" ";
-        k++;
-        visit[v]=0;
-        visited[v]=1;
+        //if the node i is unvisited
+        if (!v[i])
+        {
+            bfsTraversal(i);
+        }
     }
+
+    cout << "\n\n\n";
+
     return 0;
 }
