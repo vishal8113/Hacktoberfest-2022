593ee2100fa1cc76d93ae6a5d1d0e52734c6de80
Samrudhiiii <sgulhane112020@gmail.com>
2022-10-29 22:27:41 +0530

    Java_program_to_show_how_to_compare_Strings

---
+++ Java/CompStr.java
@@ -0,0 +1,17 @@
+// Java program to show how to compare Strings
+// using library function
+public class CompStr
+{
+    public static void main(String[] args)
+    {
+        String s1 = "Ram";
+        String s2 = "Ram";
+        String s3 = "Shyam";
+        String s4 = "ABC";
+ 
+        System.out.println(" Comparing strings with compareTo:");
+        System.out.println(s1.compareTo(s2));
+        System.out.println(s1.compareTo(s3));
+        System.out.println(s1.compareTo(s4));
+    }
+}
