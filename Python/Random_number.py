23e86e74be37626397a9848553260eca0e553a84
ragu-1997 <81232337+ragu-1997@users.noreply.github.com>
2022-10-30 19:17:11 +0530

    Create Random_number.py

---
+++ Python/Random_number.py
@@ -0,0 +1,13 @@
+# Python3 program to demonstrate the use of
+# choice() method
+
+# import random
+import random
+
+# prints a random value from the list
+list1 = [1, 2, 3, 4, 5, 6]
+print(random.choice(list1))
+
+# prints a random item from the string
+string = "striver"
+print(random.choice(string))
